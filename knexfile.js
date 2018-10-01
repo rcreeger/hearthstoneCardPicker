bf8c697ca459637f91c63c7ea7d95a5d4e13e9ab
Richard Creeger <richard.creeger@gmail.com>
2018-10-01 15:19:14 -0700

    complete

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
       client: 'pg',
       connection: {
-        database: "users",
+        database: "Hearthstone_Test",
         host: "localhost"
       },
       migrations: {
